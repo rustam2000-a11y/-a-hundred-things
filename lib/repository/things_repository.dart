0bc0a6816fae9d16e95ef55d28dd2fad3c7514f8
Nikolay Danilik <i.am.nikola7@gmail.com>
2024-12-23 12:21:57 +0400

    feat: add things repository

---
+++ lib/repository/things_repository.dart
@@ -0,0 +1,34 @@
+import 'package:injectable/injectable.dart';
+import 'package:rxdart/rxdart.dart';
+import '../model/base_card_model.dart';
+import '../network/base_data_api.dart';
+
+@LazySingleton(as: ThingsRepositoryI)
+class ThingsRepository implements ThingsRepositoryI {
+  ThingsRepository({required BaseDataApiI baseDataApi})
+      : _baseDataApi = baseDataApi;
+  final BaseDataApiI _baseDataApi;
+
+  BehaviorSubject<List<ThingsModel>>? appStateStream =
+      BehaviorSubject<List<ThingsModel>>();
+
+  @override
+  Stream<List<ThingsModel>> fetchMyContacts() {
+    _baseDataApi.fetchAllThings().listen((things) {
+      appStateStream!.sink.add(things);
+    });
+    return appStateStream!;
+  }
+
+  @disposeMethod
+  @override
+  void dispose() {
+    appStateStream?.close();
+  }
+}
+
+abstract class ThingsRepositoryI {
+  void dispose();
+
+  Stream<List<ThingsModel>> fetchMyContacts();
+}
