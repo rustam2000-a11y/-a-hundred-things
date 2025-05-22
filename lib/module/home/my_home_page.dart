675303e871513201e0d5c4311828f9e03e33a5a6
rustam <rustamz2000@gmail.com>
2025-05-22 22:04:55 +0400

    feat: creating a new home page design

--- lib/module/home/my_home_page.dart
+++ lib/module/home/my_home_page.dart
@@ -6,9 +6,11 @@ import 'package:get_it/get_it.dart';
 import '../../core/utils/presentation.utils.dart';
 import '../../generated/l10n.dart';
 import '../../presentation/colors.dart';
+import '../login/widget/custom_text.dart';
 import 'home_bloc.dart';
 import 'widget/category_card_widget.dart';
 import 'widget/drawer.dart';
+import 'widget/navigation_bar_widget.dart';
 import 'widget/new_custom_app_bar.dart';
 import 'widget/show_add_item_bottom_sheet.dart';
 import 'widget/things_card_item.dart';
@@ -38,7 +40,6 @@ class MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
-
     return BlocBuilder<HomeBloc, HomeState>(
       bloc: _bloc,
       builder: (context, state) {
@@ -48,130 +49,105 @@ class MyHomePageState extends State<MyHomePage> {
           appBar: const NewCustomAppBar(
             showBackButton :false,
           ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          body: Stack(
             children: [
-              const SizedBox(height: 16),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Text(
-                  S.of(context).ategories,
-                  style: const TextStyle(fontSize: 24),
-                ),
-              ),
-              const SizedBox(height: 8),
-              SizedBox(
-                height: 50,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.only(left: 16),
-                  children: state.typesWithColors.entries.map((entry) {
-                    final type = entry.key;
-                    final color = entry.value.isEmpty
-                        ? PresentationUtils.getRandomColor()
-                        : entry.value;
-                    return CategoryCardWidget(
-                      selectedCategoryType: _selectedCategoryType,
-                      onChangeCategory: (String? category) {
-                        setState(() {
-                          if (_selectedCategoryType == category) {
-                            _selectedCategoryType = null;
-                          } else {
-                            _selectedCategoryType = category;
-                          }
-                          _bloc.add(HomeSelectTypeThingsEvent(
-                              selectedTypeThings: _selectedCategoryType));
-                        });
-                      },
-                      onDeleteThings: () {
-                        _bloc.add(DeleteThingsByTypeEvent(type: type));
-                      },
-                      isDarkMode: isDarkMode,
-                      color: color,
-                      type: type,
-                    );
-                  }).toList(),
-                ),
-              ),
-              const SizedBox(height: 12),
-              Expanded(
-                child: ListView.builder(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  itemCount: state.things.length,
-                  itemBuilder: (context, index) {
-                    final item = state.things[index];
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 8.0),
-                      child: ThingsCardWidget(
-                        itemId: item.id,
-                        title: item.title,
-                        description: item.description,
-                        type: item.type,
-                        color: item.color,
-                        imageUrl: item.imageUrl,
-                        selectedCategoryType: _selectedCategoryType,
-                        onStateUpdate: () {
-                          setState(() {});
+              Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CustomText5(
+                          text: 'All Your Things',
+                          fontSize: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Divider(
+                    thickness: 1,
+                    height: 1,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      padding: const EdgeInsets.only(left: 16),
+                      children: state.typesWithColors.entries.map((entry) {
+                        final type = entry.key;
+                        final color = entry.value.isEmpty
+                            ? PresentationUtils.getRandomColor()
+                            : entry.value;
+                        return CategoryCardWidget(
+                          selectedCategoryType: _selectedCategoryType,
+                          onChangeCategory: (String? category) {
+                            setState(() {
+                              if (_selectedCategoryType == category) {
+                                _selectedCategoryType = null;
+                              } else {
+                                _selectedCategoryType = category;
+                              }
+                              _bloc.add(HomeSelectTypeThingsEvent(
+                                  selectedTypeThings: _selectedCategoryType));
+                            });
+                          },
+                          onDeleteThings: () {
+                            _bloc.add(DeleteThingsByTypeEvent(type: type));
+                          },
+                          isDarkMode: isDarkMode,
+                          color: color,
+                          type: type,
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 100),
+                      child: ListView.builder(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        itemCount: state.things.length,
+                        itemBuilder: (context, index) {
+                          final item = state.things[index];
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: ThingsCardWidget(
+                              itemId: item.id,
+                              title: item.title,
+                              description: item.description,
+                              type: item.type,
+                              imageUrl: item.imageUrl,
+                              selectedCategoryType: _selectedCategoryType,
+                              onStateUpdate: () {
+                                setState(() {});
+                              },
+                              quantity: item.quantity,
+                              onDeleteItem: () {
+                                _bloc.add(DeleteItemByUidEvent(uid: item.id));
+                              },
+                              selectedItemsNotifier: selectedItemsNotifier,
+                            ),
+                          );
                         },
-                        quantity: item.quantity,
-                        onDeleteItem: () {
-                          _bloc.add(DeleteItemByUidEvent(uid: item.id));
-                        },
-                        selectedItemsNotifier: selectedItemsNotifier,
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: NavigationBarWidget(isDarkMode: isDarkMode,),
               ),
             ],
           ),
-          floatingActionButton: SafeArea(
-            child: ValueListenableBuilder<List<String>>(
-              valueListenable: selectedItemsNotifier,
-              builder: (context, selectedItems, child) {
-                final isDarkTheme =
-                    Theme.of(context).brightness == Brightness.dark;
 
-                return selectedItems.isNotEmpty
-                    ? FloatingActionButton(
-                  onPressed: () {
-                    deleteSelectedItems(context);
-                  },
-                  backgroundColor: Colors.redAccent,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const Icon(Icons.delete),
-                )
-                    : Container(
-                  width: 60,
-                  height: 60,
-                  decoration: isDarkTheme
-                      ? BoxDecoration(
-                    gradient: AppColors.blueGradient,
-                    borderRadius: BorderRadius.circular(10),
-                  )
-                      : null,
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      showAddItemBottomSheet(context);
-                    },
-                    backgroundColor: isDarkMode
-                        ? Colors.transparent
-                        : AppColors.orangeSand,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    elevation: 0,
-                    child: const Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
+
         );
       },
     );
@@ -215,33 +191,4 @@ Future<void> loadTypeColorsFromFirestore() async {
   }
 }
 
-class _FixedHeaderDelegate extends SliverPersistentHeaderDelegate {
-  _FixedHeaderDelegate({
-    required this.minHeight,
-    required this.maxHeight,
-    required this.child,
-  });
 
-  final double minHeight;
-  final double maxHeight;
-  final Widget child;
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return child;
-  }
-
-  @override
-  double get maxExtent => maxHeight;
-
-  @override
-  double get minExtent => minHeight;
-
-  @override
-  bool shouldRebuild(_FixedHeaderDelegate oldDelegate) {
-    return oldDelegate.maxHeight != maxHeight ||
-        oldDelegate.minHeight != minHeight ||
-        oldDelegate.child != child;
-  }
-}
