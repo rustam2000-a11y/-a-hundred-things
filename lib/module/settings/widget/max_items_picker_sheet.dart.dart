a816764555b9c5af77f8d04fe29ed7cf74e3876d
rustam <rustamz2000@gmail.com>
2025-07-16 19:20:29 +0400

    refactor: Updated design and logic for selecting the maximum number of elements

--- lib/module/settings/widget/max_items_picker_sheet.dart.dart
+++ lib/module/settings/widget/max_items_picker_sheet.dart.dart
@@ -1,80 +1,124 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../login/widget/button_basic.dart';
+import '../bloc/max_items_bloc/max_items_bloc.dart';
 
-class MaxItemsPickerSheet extends StatelessWidget {
+class MaxItemsPickerSheet extends StatefulWidget {
   const MaxItemsPickerSheet({
     super.key,
-    required this.initialValue,
     required this.onSelected,
+    required this.onHideNavChanged,
+    required this.onValueChanged,
   });
 
-  final int initialValue;
   final ValueChanged<int> onSelected;
+  final ValueChanged<bool> onHideNavChanged;
+  final ValueChanged<int> onValueChanged;
+  @override
+  State<MaxItemsPickerSheet> createState() => _MaxItemsPickerSheetState();
+}
+
+class _MaxItemsPickerSheetState extends State<MaxItemsPickerSheet> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final bloc = context.read<MaxItemsBloc>();
+    _controller = TextEditingController();
+
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final state = bloc.state;
+      _controller.text = state.maxItems.toString();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final values = List.generate(81, (index) => 100 - index);
-    int tempValue = initialValue;
-    final initialIndex = values.indexOf(initialValue);
+    return BlocListener<MaxItemsBloc, MaxItemsState>(
+      listener: (context, state) {
+        widget.onHideNavChanged(state.isSpecial);
+        widget.onValueChanged(state.maxItems);
+        if (state.done) {
+          widget.onSelected(state.maxItems);
+          Navigator.of(context).pop();
+        }
 
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Expanded(
-            child: CupertinoPicker(
-          scrollController:
-              FixedExtentScrollController(initialItem: initialIndex),
-          itemExtent: 40,
-          magnification: 1.2,
-          squeeze: 1.1,
-          useMagnifier: true,
-          backgroundColor: Colors.transparent,
-          onSelectedItemChanged: (index) {
-            tempValue = values[index];
-          },
-          selectionOverlay: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 15),
-            decoration: BoxDecoration(
-              border: Border.all(),
-              color: Colors.transparent,
-            ),
-          ),
-          children: values
-              .map((value) => Center(
-                    child: Text(
-                      value.toString(),
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Theme.of(context).textTheme.bodyLarge?.color,
-                      ),
+        if (_controller.text != state.maxItems.toString()) {
+          _controller.text = state.maxItems.toString();
+        }
+      },
+      child:BlocBuilder<MaxItemsBloc, MaxItemsState>(
+        buildWhen: (previous, current) =>
+        previous.maxItems != current.maxItems ||
+            previous.errorText != current.errorText,
+        builder: (context, state) {
+          return Padding(
+            padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: TextField(
+                    controller: _controller,
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {
+                      final parsed = int.tryParse(value.trim()) ?? 0;
+                      context.read<MaxItemsBloc>().add(MaxItemsChangedEvent(parsed));
+                    },
+                    decoration: InputDecoration(
+                      errorText: state.errorText,
+                      border: const OutlineInputBorder(),
                     ),
-                  ))
-              .toList(),
-        )),
-        SizedBox(
-          width: 150,
-          child: CustomMainButton(
-            text: 'Select',
-            onPressed: () async {
-              final userId = FirebaseAuth.instance.currentUser?.uid;
-              if (userId != null) {
-                await FirebaseFirestore.instance
-                    .collection('user')
-                    .doc(userId)
-                    .set({'maxItems': tempValue}, SetOptions(merge: true));
-              }
+                  ),
+                ),
 
-              onSelected(tempValue);
-              Navigator.pop(context);
-            },
-          ),
-        ),
-        const SizedBox(height: 10),
-      ],
+
+                BlocBuilder<MaxItemsBloc, MaxItemsState>(
+                  buildWhen: (previous, current) =>
+                  previous.isSpecial != current.isSpecial,
+                  builder: (context, state) {
+                    return CheckboxListTile(
+                      title: const Text('hide item count indicator'),
+                      value: state.isSpecial,
+                      onChanged: (bool? value) {
+                        context.read<MaxItemsBloc>().add(
+                          MaxItemsSpecialChangedEvent(value ?? false),
+                        );
+                      },
+                      controlAffinity: ListTileControlAffinity.leading,
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 24),
+                    );
+                  },
+                ),
+
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: 150,
+                  child: CustomMainButton(
+                    text: 'Select',
+                    onPressed: () {
+                      context.read<MaxItemsBloc>().add(const MaxItemsSubmittedEvent());
+                    },
+                  ),
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
+
